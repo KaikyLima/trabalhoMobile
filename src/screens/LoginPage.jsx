64c98f95ed50c30b349db674241a0be17bbcb5fd
Kaikyl <kaikylimasouza@gmail.com>
2023-08-25 20:40:38 -0300

    update

---
+++ src/screens/LoginPage.jsx
@@ -0,0 +1,17 @@
+import { SafeAreaView } from "react-native";
+import React, { useState } from 'react';
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');   
+}
+
+const LoginPage = () => {
+    return (
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    );
+}
+
+export default LoginPage;
