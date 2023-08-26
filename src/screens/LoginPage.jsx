2e81f2bb8534a3b233e97bdbd6dd1fc5aa574784
Kaikyl <kaikylimasouza@gmail.com>
2023-08-25 21:04:53 -0300

    Tela login

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -1,17 +1,40 @@
-import { SafeAreaView } from "react-native";
+import { Alert, SafeAreaView, TextInput, Text, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
 
-const LoginScreen = () => {
+const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');   
-}
 
-const LoginPage = () => {
-    return (
+
+const verificarLogin = () => {
+    if (email.trim() === '' || password.trim() === '') {
+      Alert.Alert('O preenchimento dos campos é obrigatório');
+    }
+  };
+
+     return (
         <SafeAreaView>
-            
+            <TextInput 
+            style={styles.input}
+            onChangeText= {setEmail}
+            value = {email}
+        />
+        <TextInput 
+            style={styles.input}
+            onChangeText= {setPassword}
+            value = {password}
+        />
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 60,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
 
 export default LoginPage;
