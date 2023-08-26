1d675209e200f2670a72939e94062a69252375d8
Kaikyl <kaikylimasouza@gmail.com>
2023-08-25 21:15:15 -0300

    Tela de login update 2.0

--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Alert, SafeAreaView, TextInput, Text, StyleSheet } from 'react-native';
+import { Alert, SafeAreaView, TextInput, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 
 const LoginPage = () => {
@@ -8,22 +8,31 @@ const LoginPage = () => {
 
 const verificarLogin = () => {
     if (email.trim() === '' || password.trim() === '') {
-      Alert.Alert('O preenchimento dos campos é obrigatório');
+      Alert.alert('O preenchimento dos campos é obrigatório');
     }
   };
 
      return (
         <SafeAreaView>
             <TextInput 
+            placeholder="Email"
             style={styles.input}
             onChangeText= {setEmail}
             value = {email}
         />
         <TextInput 
+        placeholder="Senha"
             style={styles.input}
             onChangeText= {setPassword}
             value = {password}
         />
+
+        <TouchableOpacity style={styles.button} onPress={verificarLogin}>
+
+            <Text>Logar</Text>
+
+        </TouchableOpacity>
+
         </SafeAreaView>
     );
 }
@@ -35,6 +44,9 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       padding: 10,
     },
+    button: {
+        backgroundColor:"#AAA"
+    }
   });
 
 export default LoginPage;
