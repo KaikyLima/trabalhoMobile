f067a118e51db61665a234a4fd16f2f6dba0a2ef
Kaikyl <kaikylimasouza@gmail.com>
2023-08-29 20:45:21 -0300

    Home 2.0

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
+import Home from './src/screens/Home';
 import LoginPage from './src/screens/LoginPage';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <LoginPage/>
+    <Home/>
   );
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,57 +1,46 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
-import { useState } from "react";
-import { TouchableOpacity, Text } from "react-native";
-
-const Pagination = ({request, offset}) => {
-
-    const nextPage = () => {
-        request(offset + 10)
-    }
-
-    const previousPage = () => {
-        if (offset <= 10) return
-        request(offset - 10)
-    }
-
-    return (
-        <view>
-            <TouchableOpacity>
-            <Text onClick={previousPage}> Carregar menos </Text>
-            </TouchableOpacity>
-            
-            <TouchableOpacity>
-            <Text onClick={nextPage}> Carregar mais </Text>
-            </TouchableOpacity>
-            
-        </view>
-    )
-
-    
-}
 
 const Home = () => {
+  const [csgo, setCsgo] = useState([]);
 
+  const fetchCsgoData = useCallback(async () => {
+    try {
+      const { data } = await axios.get(
+        "https://cs2-api.vercel.app/api/items?page=1"
+      );
+      setCsgo(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
-const fetchCsgoData = useCallback (async (paramOffset) =>{
-    const [offset, setOffset] = useState(10);
-    const [csgo, setCsgo] = useState ([]);
+  useEffect(() => {
+    fetchCsgoData();
+  }, []);
 
-        try{
-            const{data} = await axios.get('https://cs2-api.vercel.app/api/items?page=1')
-            setCsgo(data)
-        }catch(error){
-            console.error(error)
-        }
+  const renderItem = ({ item }) => {
+    return <Text style={styles.itemText}>{item.name}</Text>;
+  };
 
-    },[])
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList data={csgo} renderItem={renderItem} />
+    </SafeAreaView>
+  );
+};
 
-    useEffect(() => {
-        fetchCsgoData()
-    },[])
-
-    return (
-
-    );
-}
+const styles = {
+  container: {
+    backgroundColor: "#f0f0f0", 
+  },
+  itemText: {
+    color: "black", 
+    fontSize: 16,
+    padding: 10,
+  },
+};
 
 export default Home;
