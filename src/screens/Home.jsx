72ff36a6f6ee58202d5ed3774b8e8a14279c9269
Kaikyl <kaikylimasouza@gmail.com>
2023-08-30 20:05:03 -0300

    home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
 
@@ -8,10 +8,10 @@ const Home = () => {
 
   const fetchCsgoData = useCallback(async () => {
     try {
-      const { data } = await axios.get(
+      const { data: response } = await axios.get(
         "https://cs2-api.vercel.app/api/items?page=1"
       );
-      setCsgo(data);
+      setCsgo(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -22,25 +22,41 @@ const Home = () => {
   }, []);
 
   const renderItem = ({ item }) => {
-    return <Text style={styles.itemText}>{item.name}</Text>;
+    
+    return  (
+      <View>
+    <Text style={styles.itemText}>{item.name}</Text>
+    <Image source={{uri:item.image}} style={styles.imageCss}/>
+    <Text>{item.rarity}</Text>
+    </View>
+    )
+
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList data={csgo} renderItem={renderItem} />
+
     </SafeAreaView>
   );
 };
 
-const styles = {
+const styles = StyleSheet.create =( {
   container: {
-    backgroundColor: "#f0f0f0", 
+    backgroundColor: "#f0fdff", 
+
   },
   itemText: {
     color: "black", 
     fontSize: 16,
     padding: 10,
   },
-};
+  imageCss: {
+    width: 100,
+    height: 100,
+  }
+});
+
+console.log("teste")
 
 export default Home;
