d3356bc7c962622a538fe7b85b1b657a5dfe4851
Kaikyl <kaikylimasouza@gmail.com>
2023-09-05 20:39:38 -0300

    a

--- src/screens/Detalhes.jsx
+++ src/screens/Detalhes.jsx
@@ -1,66 +1,63 @@
-import { useCallback, useEffect, useState } from "react";
-import { TouchableOpacity } from "react-native";
-import { TouchableOpacityComponent } from "react-native";
-import { FlatList, Image, SafeAreaView, StyleSheet, View, Text} from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet, SafeAreaView } from "react-native";
 import axios from "axios";
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f0fdff",
+  },
+  itemText: {
+    color: "black",
+    fontSize: 16,
+    padding: 10,
+  },
+  imageCss: {
+    width: 360,
+    height: 400,
+  },
+  imageCssColection: {
+    width: 100,
+    height: 100,
+  },
+});
 
-const Detalhes = ({route}) => {
+const DetalhesItem = ({ route }) => {
+  const { id } = route.params;
+  const [itemDetails, setItemDetails] = useState(null);
 
-    console.log(route.params.id)
+  useEffect(() => {
+    const fetchItemDetails = async () => {
+      try {
+        const { data: response } = await axios.get(
+          `https://cs2-api.vercel.app/api/items/?id=${id}`
+        );
+        console.log(response)
+        setItemDetails(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    const styles = StyleSheet.create({
-        container: {
-          backgroundColor: "#f0fdff", 
-      
-        },
-        itemText: {
-          color: "black", 
-          fontSize: 16,
-          padding: 10,
-        },
-        imageCss: {
-          width: 100,
-          height: 100,
-        }
-      });
+    fetchItemDetails();
+  }, [id]);
 
-    const [detalhe, setDetalhes] = useState([])
-
-    const fetchDetalhesData = useCallback(async () => {
-        try{
-            const { data: response } = await axios.get(
-                "https://cs2-api.vercel.app/api/items"
-            );
-            setDetalhes(response.data);
-        }catch(error){
-            console.error(error);
-        }
-    }, [])
-
-    useEffect(() => {
-        fetchDetalhesData();
-      }, []);
-
-      
-
-      const renderItem = ({ item }) => {
-        return  (
+  return (
+    <SafeAreaView>
+    <View style={styles.container}>
+      {itemDetails && (
+        <>
           <View>
-            <Text style={styles.itemText}>{item.name}</Text>
-            <Image source={{uri:item.image}} style={styles.imageCss}/>
-            <Text>{item.rarity}</Text>
-            <Text>{item.description}</Text>
+            <Text>Coleção: {itemDetails.collections[0].name}</Text>
+            <Image source={{ uri: itemDetails.collections[0].image }} style={styles.imageCssColection} />
+
+            
           </View>
-        )
-      };
+        </>
+      )}
+    </View>
+    </SafeAreaView>
+    
+  );
+};
 
-    return (
-        <SafeAreaView>
-             <FlatList data={detalhe} renderItem={renderItem} />
-        </SafeAreaView>
-        
-    );
-}
-
-export default Detalhes;
+export default DetalhesItem;
